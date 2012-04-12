a2df4efb691cbb90d038fc444be10c1914792369
Eric Shull <eric.shull@gmail.com>
2012-04-12 19:03:26 -0400

    Handle OPTION requests.

--- zipapiserver.py
+++ zipapiserver.py
@@ -46,6 +46,11 @@ class ZipAPIServerHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                 s.end_headers()
                 s.wfile.write("404 - Not Found")
 
+    def do_OPTION(s):
+        s.send_response(200)
+        s.send_header("Access-Control-Allow-Origin", "*")
+        s.end_headers()
+
 
 if __name__ == '__main__':
     server_class = BaseHTTPServer.HTTPServer
