de18acb1f91b95a1bca81e4c043fd6b1e961c7e9
Thomas Schultz <daspecster@gmail.com>
2012-05-08 18:28:36 -0400

    Added v2 and 60 country database

--- zipapiserver.py
+++ zipapiserver.py
@@ -5,8 +5,8 @@ import json
 import sqlite3
 
 
-HOST_NAME = 'localhost' # !!!REMEMBER TO CHANGE THIS!!!
-PORT_NUMBER = 80 # Maybe set this to 9000.
+HOST_NAME = 'localhost'
+PORT_NUMBER = 80
 
 class ZipAPIServerHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(s):
@@ -14,39 +14,52 @@ class ZipAPIServerHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         qs = {}
         path = s.path
         the_zip = None
+        the_country = None
+        database_name = "zipcodes-old.db"
+
+        if path.startswith("/v2"):
+            database_name = "zipcodes.db"
+            path = path[3:]
+
         if '?' in path:
             path, tmp = path.split('?', 1)
             qs = urlparse.parse_qs(tmp)
             the_zip = qs['zip'][0]
         elif path:
             the_zip = path.strip('/')
+        
+        p = path.lstrip("/")
+        p = p.split("/", 2)
+
+        if len(p) == 2:
+            the_zip = p[1]
+            the_country = p[0]
 
         the_zip = [the_zip.split('-')[0]]
 
         if the_zip:
             # Query database with the ZIP and pull the city, state, country
-            conn = sqlite3.connect('zipcodes.db')
+            conn = sqlite3.connect(database_name)
             c = conn.cursor()
-
-            c.execute('select Country, State, City from zipcodes where ZipCode=?', the_zip)
+            c.execute("SELECT Country, State, City from zipcodes WHERE zipcode LIKE ?", the_zip)
 
             row = c.fetchone()
+
             if row is not None:
                 s.send_response(200)
-                # The Magic!
                 s.send_header("Access-Control-Allow-Origin", "*")
                 s.send_header("Content-type", "application/json")
                 s.end_headers()
-        
-                data = dict(zip(('country', 'state', 'city'), row))
+
+                data = {'country': str(row[0]),'state': str(row[1]), 'city': str(row[2])}
+
                 s.wfile.write(json.dumps(data))
             else:
-                # The Magic!
                 s.send_response(404)
                 s.send_header("Access-Control-Allow-Origin", "*")
                 s.send_header("Content-type", "text/plain")
                 s.end_headers()
-                s.wfile.write("404 - Not Found")
+                s.wfile.write("{}")
 
     def do_OPTIONS(s):
         s.send_response(200)
@@ -55,7 +68,7 @@ class ZipAPIServerHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         s.end_headers()
 
 
-if __name__ == '__main__':
+def start_server():
     server_class = BaseHTTPServer.HTTPServer
     httpd = server_class((HOST_NAME, PORT_NUMBER), ZipAPIServerHandler)
     #print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
@@ -64,3 +77,6 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         pass
     httpd.server_close()
+
+if __name__ == '__main__':
+    start_server()
