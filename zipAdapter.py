374e543f0e47a9e3745c61b188548c0e4e4305ac
Thomas Schultz <daspecster@gmail.com>
2012-05-08 18:28:36 -0400

    Added v2 and 60 country database

--- zipAdapter.py
+++ zipAdapter.py
@@ -1,37 +1,31 @@
+import sys
 import csv
 import sqlite3
+import thread   
+
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+file_len('allCountries.txt') / 2
 
 conn = sqlite3.connect("zipcodes.db")
 c = conn.cursor()
 
-c.execute('''create table if not exists zipcodes (id integer primary key, zipcode integer, city text, state text, country text)''')
+c.execute('''create table if not exists zipcodes (id integer primary key, zipcode text, city text, state text, country text)''')
 
 
-zipAdapter = csv.reader(open('free-zipcode-database.csv', 'rb'), delimiter=",", quotechar="'")
+zipAdapter = csv.reader(open('allCountries.txt', 'rb'), delimiter="\t")
+
+csv.field_size_limit(sys.maxint)
+
 for row in zipAdapter:
-    #insert_string = '''insert into zipcodes values(NULL, {0}, '{1}', '{2}', '{3}')'''.format(row[1].strip('"'), 
-    #	                                                                           row[3].strip('"'), 
-    #	                                                                           row[4].strip('"'), 
-    #	                                                                           row[12].strip('"'))
-    #print insert_string
-
     try:
-
-        c.execute('''insert into zipcodes values(NULL, ?, ?, ?, ?)''',(row[1].strip('"'), 
-    	                                                                           row[3].strip('"'), 
-    	                                                                           row[4].strip('"'), 
-    	                                                                           row[12].strip('"')))
+        c.execute(u"insert into zipcodes values(NULL, ?, ?, ?, ?)",(buffer(row[1]), buffer(row[2]), buffer(row[3]), buffer(row[0])))
     except Exception as e:
     	print e.message
     	break
     conn.commit()
-
-
-	#print row[12].strip('"')
-	#print row[1].strip('"')
-	
-	#print row[3].strip('"')
-	#print row[4].strip('"')
-    
-
 c.close()
